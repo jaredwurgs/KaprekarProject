1d159cd65e108c3917db9a1da167ea08e6ad7eda
Jared <elliotta@DREN-WS21.AFWA.HPC.MIL>
2018-09-07 13:00:47 -0500

    Update Message

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -1,4 +1,5 @@
 #include<math.h>
+#include<stdio.h>
 
 #include "kaprekarUtils.h"
 
@@ -25,21 +26,21 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
-  int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  int numDigits = (int) log10(square) + 1;
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -48,7 +49,7 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
